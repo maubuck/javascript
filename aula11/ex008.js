bbcc86a0ebe654a8de579a0da60de1f70c2c9fa9
maubuck <123389153+maubuck@users.noreply.github.com>
2023-05-15 14:48:07 -0300

    condições em js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if (vel>60) {
+    console.log(`Você ultrapassou o limitede de velocidade, MULTADO!`)
+}
+console.log(`Dirija sempre udando cinto de segurança`)
